336fa5cc2ae10599dd62d4f40f0ac2fb45dd92d5
Smit Kanojiya <smit.kanojiya@vvdntech.in>
2023-08-31 14:19:58 +0530

    Initial Commit

---
+++ OnBoarding/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OnBoarding
+//
+//  Created by Smit Kanojiya on 31/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by Smit Kanojiya on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
